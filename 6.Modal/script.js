fab27e8e3ab8d8ed74ea91d8dc0373651e045c65
Subrat Garnaik <122955567+JS-Swan@users.noreply.github.com>
2024-09-29 07:23:42 +0530

    Add files via upload

---
+++ 6.Modal/script.js
@@ -0,0 +1,20 @@
+const openModalBtn=document.querySelector(".open-modal");
+const modalBackground=document.querySelector(".modal-background");
+const closeIcon=document.querySelector(".close-icon");
+const closeBtn=document.querySelector(".close");
+openModalBtn.addEventListener("click",()=>{
+    modalBackground.style.display="block";
+})
+closeIcon.addEventListener("click",()=>{
+    modalBackground.style.display="none";
+})
+closeBtn.addEventListener("click",()=>{
+    modalBackground.style.display="none";
+})
+
+window.addEventListener("click",(event)=>{
+    // console.log(event.target)
+    if(event.target === modalBackground){
+        modalBackground.style.display="none";  
+    }
+})
